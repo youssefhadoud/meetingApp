cf08f8687f668f6e71855b28ed32506444cc5534
youssef <youssefhadoud1234@gmail.com>
2025-02-10 22:11:11 +0100

    fix the bigbluebutton and create the controller for geree les meeting

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MeetingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('meeting', MeetingController::class);
+
+Route::get('check',function(){
+    dd(\Bigbluebutton::isConnect());
 });
